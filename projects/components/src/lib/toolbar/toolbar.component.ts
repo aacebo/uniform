4975d97f80bad26deb4d9a1091b428a45816490a
pappacurds <aacebowork@gmail.com>
2019-05-19 17:45:52 -0400

    fix toolbar to use host context

--- projects/components/src/lib/toolbar/toolbar.component.ts
+++ projects/components/src/lib/toolbar/toolbar.component.ts
@@ -6,6 +6,14 @@ import { UniColor } from '../core/enums';
   selector: 'uni-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
+  host: {
+    class: 'uni-toolbar',
+    '[class.primary]': 'color === "primary"',
+    '[class.secondary]': 'color === "secondary"',
+    '[class.success]': 'color === "success"',
+    '[class.warning]': 'color === "warning"',
+    '[class.danger]': 'color === "danger"'
+  },
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UniToolbarComponent {
