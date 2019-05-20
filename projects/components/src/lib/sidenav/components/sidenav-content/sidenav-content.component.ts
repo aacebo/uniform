ce612fbfb25adfd1feabd90cc57e3a3bba89c4f6
Alex Acebo <alexander.acebo@semanticbits.com>
2019-05-19 23:24:53 -0400

    Update sidenav-content.component.ts

--- projects/components/src/lib/sidenav/components/sidenav-content/sidenav-content.component.ts
+++ projects/components/src/lib/sidenav/components/sidenav-content/sidenav-content.component.ts
@@ -8,13 +8,34 @@ import { UniSidenavContainerComponent } from '../sidenav-container/sidenav-conta
   styleUrls: ['./sidenav-content.component.scss'],
   host: {
     class: 'uni-sidenav-content',
-    '[style.margin-left.px]': 'container.sidenav.open ? container.sidenav.el.nativeElement.clientWidth : 0'
+    '[style.margin-left.px]': 'marginLeft',
+    '[style.margin-right.px]': 'marginRight',
   },
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UniSidenavContentComponent {
+  get marginLeft() {
+    return this.open && this.position === 'start' ? this.width : 0;
+  }
+
+  get marginRight() {
+    return this.open && this.position === 'end' ? this.width : 0;
+  }
+
+  get open() {
+    return this.container.sidenav.open;
+  }
+
+  get position() {
+    return this.container.sidenav.position;
+  }
+
+  get width() {
+    return +this.container.sidenav.el.nativeElement.clientWidth;
+  }
+
   constructor(
     @Inject(forwardRef(() => UniSidenavContainerComponent))
-    readonly container: UniSidenavContainerComponent,
+    private readonly container: UniSidenavContainerComponent,
   ) {}
 }
