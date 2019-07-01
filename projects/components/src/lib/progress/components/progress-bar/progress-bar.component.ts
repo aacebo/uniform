3236d10ee95cf06f665a79d9025ff180a4ae9eb9
Alex Acebo <alexander.acebo@semanticbits.com>
2019-07-01 17:00:00 -0400

    add basic progress bar

--- projects/components/src/lib/progress/components/progress-bar/progress-bar.component.ts
+++ projects/components/src/lib/progress/components/progress-bar/progress-bar.component.ts
@@ -1,4 +1,8 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+
+import { UniProgressMode } from '../../enums/progress-mode.enum';
+import { UniColor } from '../../../core/enums';
+import { uniColors } from '../../../core/constants';
 
 @Component({
   moduleId: module.id,
@@ -7,8 +11,22 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   templateUrl: './progress-bar.component.html',
   styleUrls: ['./progress-bar.component.scss'],
   host: {
-    class: 'uni-progress-bar'
+    class: 'uni-progress-bar',
+    '[class.indeterminate]': 'mode === "indeterminate"'
   },
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UniProgressBarComponent {}
+export class UniProgressBarComponent {
+  @Input() mode = UniProgressMode.Indeterminate;
+  @Input() color = UniColor.Primary;
+  @Input() total = 100;
+  @Input() value = 0;
+
+  get colors() {
+    return uniColors(this.color);
+  }
+
+  get percentage() {
+    return (100 / this.total) * this.value;
+  }
+}
